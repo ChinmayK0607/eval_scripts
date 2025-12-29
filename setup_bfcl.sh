d82c9709538daa588b4c9e8de95d2afc9af48c0c
ChinmayK0607 <114411195+ChinmayK0607@users.noreply.github.com>
2025-12-29 11:17:31 +0530

    Update setup_bfcl.sh

--- setup_bfcl.sh
+++ setup_bfcl.sh
@@ -10,11 +10,24 @@ cd gorilla/berkeley-function-call-leaderboard
 uv pip install -e .
 uv pip install -e ".[oss_eval_vllm]"
 
-cp -n bfcl_eval/.env.example .env || true
+cp bfcl_eval/.env.example .env || true
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
-vllm serve Qwen/Qwen3-8B \
+vllm serve Qwen/Qwen2.5-7B-Instruct \
   --tensor-parallel-size 4 \
   --host 0.0.0.0 \
   --port 8000 \
   --dtype auto
+
+
+  bfcl generate --model Qwen/Qwen2.5-7B-Instruct-FC --test-category single_turn --skip-server-setup
+
+
+bfcl generate \
+--model Qwen/Qwen2.5-7B-Instruct-FC \
+--test-category simple_python \
+--backend vllm \
+--num-gpus 4 \
+--gpu-memory-utilization 0.8
+
+bfcl evaluate --model Qwen/Qwen2.5-7B-Instruct-FC --test-category single_turn
